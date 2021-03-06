ee8bd146ce67c60ee6cede31a873bc82658c0581
Andrey <example@gmail.com>
2021-03-06 16:25:03 +0600

    new test github

--- JAVASCRIPT/Project/app.js
+++ JAVASCRIPT/Project/app.js
@@ -1,3 +1,23 @@
-alert("Hello")
-alert("Hello")
-alert("Hello")
+'use strict';
+
+
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?")
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: " ",
+    actors: " ",
+    genres: " ",
+    privat: false
+}
+
+
+const movies = {
+   
+}
+movies.whichFilm = prompt("Один из последних просмотренных фильмов")
+movies.countFilm = prompt("На сколько оцените его?")
+console.log(personalMovieDB);
+console.log(movies);
+
