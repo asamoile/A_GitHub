2c3f9da610e61391aba24d28c1622eb11cdf7856
Andrey <example@gmail.com>
2021-03-06 16:46:55 +0600

    MovieDB created

--- JAVASCRIPT/Project/app.js
+++ JAVASCRIPT/Project/app.js
@@ -1,23 +1,21 @@
 'use strict';
-
-
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?")
-
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?")
 
 const personalMovieDB = {
     count: numberOfFilms,
-    movies: " ",
-    actors: " ",
-    genres: " ",
+    movies: {},
+    actors: {},
+    genres: [],
     privat: false
 }
 
+const a = prompt("Один из последних просмотренных фильмов")
+const b = +prompt("На сколько оцените его?")
+const c = prompt("Один из последних просмотренных фильмов")
+const d = +prompt("На сколько оцените его?")
 
-const movies = {
-   
-}
-movies.whichFilm = prompt("Один из последних просмотренных фильмов")
-movies.countFilm = prompt("На сколько оцените его?")
+personalMovieDB.movies[a] = b
+personalMovieDB.movies[c] = d
+
 console.log(personalMovieDB);
-console.log(movies);
 
