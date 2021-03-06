86a4ab08a9335c176e2781c1c44309fe9254177d
Andrey <example@gmail.com>
2021-03-07 00:05:05 +0600

    practice

--- JAVASCRIPT/Project/app.js
+++ JAVASCRIPT/Project/app.js
@@ -8,14 +8,34 @@ const personalMovieDB = {
     genres: [],
     privat: false
 }
+// 1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+for (let i = 0; i < 2; i++) {
+   const a = prompt("Один из последних просмотренных фильмов")
+   const b = +prompt("На сколько оцените его?")
+   // если пользователь нажал отмена это null.
+   // проверяем нажал ли пользователь отмену в первом и втором вопросе 
+   // проверяем пустые ли строки и на макс кол-во введенных символов
+   // чтобы вернуться в цикле на 1 вопрос назад, делаем декримент i
+   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b
+      console.log('done');
+   } else {
+      console.log('error');
+      i--;
+   }
+}
 
-const a = prompt("Один из последних просмотренных фильмов")
-const b = +prompt("На сколько оцените его?")
-const c = prompt("Один из последних просмотренных фильмов")
-const d = +prompt("На сколько оцените его?")
-
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
+if (personalMovieDB.count < 10) {
+   console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+   console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30){
+   console.log("Вы киноман");
+} else {
+   console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
+
+
 
